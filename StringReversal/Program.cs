fbaffbde7675486d3e03748571dd380c035822bf
Cristian Pisco <cristian.pisco@outlook.com>
2022-02-23 19:45:32 -0500

    leet code coding challenge

---
+++ StringReversal/Program.cs
@@ -0,0 +1,16 @@
+﻿string value = "The simple engineer";
+// var result = StringReversal(value, value.Length - 1);
+var result = StringReversalV2(value);
+System.Console.WriteLine(result);
+
+static string StringReversal(string value, int offset)
+{
+  if (offset <= 0) return value[offset].ToString();
+  return value[offset].ToString() + StringReversal(value, --offset);
+}
+
+static string StringReversalV2(string value)
+{
+  if (string.IsNullOrWhiteSpace(value)) return string.Empty;
+  return StringReversalV2(value.Substring(1)) + value[0].ToString();
+}
